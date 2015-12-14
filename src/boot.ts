4fa51f54307a5dfb5fed2b2484333a1c76dbeb92
André Kirchner <andre@andrekirchner.com>
2015-12-14 11:18:47 +0100

    minor changes to the bootstrapper;

--- src/boot.ts
+++ src/boot.ts
@@ -1,11 +1,17 @@
 import {bootstrap, Component} from 'angular2/angular2';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+import {App} from './component/app/app';
+
 @Component({
-	selector: 'my-app',
-	template: '<h1>My First Angular 2 App</h1>'
+	selector   : 'my-app',
+	template   : '<h1>My First Angular 2 App</h1><br><supergut>Super-Gut</supergut>',
+	directives : [App]
 })
-class AppComponent {
+class BootstrapComponent {
 	constructor() {
 		console.log('Component Controller is also loading');
 	}
 }
-bootstrap(AppComponent);
+
+bootstrap(BootstrapComponent);
